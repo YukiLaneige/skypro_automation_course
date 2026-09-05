9d7366a55e9823ccb5928c64b5636f2b1821a2fe
YukiLaneige <ladvoronovyan@gmail.com>
2026-09-05 23:32:27 +0600

    Исправлено задание 3.

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = "Ян"
-last_name = "Ладворонов"
-print(last_name + " " + first_name)
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print("Вас зовут: " + last_name + " " + first_name)
