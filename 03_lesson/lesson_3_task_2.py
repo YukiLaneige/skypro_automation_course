9b721d936c8e37a0c083d9c6ce21acde038e8860
YukiLaneige <ladvoronovyan@gmail.com>
2026-09-08 22:25:24 +0600

    Исправлен таск 2

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -3,6 +3,8 @@ from smartphone import Smartphone
 catalog = [Smartphone('Apple', 'iPhone 15', '88005553535'),
            Smartphone('Samsung', 'Galaxy S24', '88001001001'),
            Smartphone('Xiaomi', 'Redmi Note 13', '88002006040'),
+           Smartphone('Google', 'Pixel 8 Pro', '89999999999'),
+           Smartphone('Nokia', '3310', '84206942069'),
            ]
 for smartphone in catalog:
     print(f"{smartphone.brand} - {smartphone.model}. {smartphone.number}")
